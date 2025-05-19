23b9463f4b686a716b62f25cd86e00ecc6f468bb
Soglo3006 <ablbooh0@gmail.com>
2025-05-19 15:40:00 -0400

    Modification de la barre

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,31 @@
 import { useState } from 'react'
 import './App.css'
+import { FaPlus, FaImage } from 'react-icons/fa';
+
 
 function App() {
   const [content,setContent] = useState("")
 
   return (
-    <div>
+    <div className="container">
       <h3>Fake News Detector</h3>
+      <div className='input-bar'>
       <form><input 
                 type="text" 
                 name="article" 
                 placeholder="Enter your news article"
+                className="text-input"
                 value={content}
                 onChange={(e) => setContent(e.target.value)} 
         /></form>
         <button className="icon-button">
+          <FaImage />
         </button>
         <button className="icon-button">
+          <FaPlus />
         </button>
+        <button className="analyze-button">Analyser</button>
+        </div>
     </div>
   )
 }
