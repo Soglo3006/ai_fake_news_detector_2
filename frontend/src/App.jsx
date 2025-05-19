0acf2f9cb866d51a55101b80f99ba9ce53446aac
Soglo3006 <ablbooh0@gmail.com>
2025-05-19 12:39:07 -0400

    creation de la page d'accueil

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [content,setContent] = useState("")
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div>
+      <h3>Fake News Detector</h3>
+      <form><input 
+                type="text" 
+                name="article" 
+                placeholder="Enter your news article"
+                value={content}
+                onChange={(e) => setContent(e.target.value)} 
+        /></form>
+        <button className="icon-button">
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        <button className="icon-button">
+        </button>
+    </div>
   )
 }
 
