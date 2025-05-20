66a45cbd12c71369084012766bbfe763abc36cf3
Soglo3006 <ablbooh0@gmail.com>
2025-05-20 16:38:03 -0400

    code html de la page de connexion

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login() {
-  return (
-    <div style={{ padding: "40px", textAlign: "center" }}>
-      <h2>Page de connexion</h2>
-      <p>Ici, tu vas pouvoir te connecter.</p>
-    </div>
-  );
+    const [email,SetEmail] = useState("");
+    const [password,SetPassword] = useState("");
+
+
+    return(
+        <div>
+            <h2>Fake News Detector</h2>
+            <h3>Connexion à votre compte</h3>
+            <input 
+            type="text" 
+            name="mail" 
+            placeholder="Entrer votre courriel"
+            value={email}
+            onChange={(e) => SetEmail(e.target.value)}
+            ></input><br></br>
+            <input
+            type="text"
+            name="mot-passe"
+            placeholder="Entrer votre mot de passe"
+            value={password}
+            onChange={(e) => SetPassword(e.target.value)}
+            ></input><br></br>
+            <button>
+                Continuer
+            </button><br></br>
+            <p>Aucun compte</p>
+        </div>
+    );
 }
 
 export default Login;
