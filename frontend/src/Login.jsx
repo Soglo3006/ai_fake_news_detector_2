c9fea3cce7d7045b2e581ec9ce2fb27d60d4e754
Soglo3006 <ablbooh0@gmail.com>
2025-05-22 00:37:20 -0400

    Ajout du code Css dans Login.jsx

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import "./Login.css";
 
 function Login() {
     const [email,SetEmail] = useState("");
@@ -7,27 +8,35 @@ function Login() {
 
 
     return(
-        <div>
+        <div className="Tab-connexion">
             <h2>Fake News Detector</h2>
-            <h3>Connexion à votre compte</h3>
-            <input 
-            type="text" 
-            name="mail" 
-            placeholder="Entrer votre courriel"
-            value={email}
-            onChange={(e) => SetEmail(e.target.value)}
-            ></input><br></br>
-            <input
-            type="text"
-            name="mot-passe"
-            placeholder="Entrer votre mot de passe"
-            value={password}
-            onChange={(e) => SetPassword(e.target.value)}
-            ></input><br></br>
-            <button>
-                Continuer
-            </button><br></br>
-            <p>Aucun compte? <Link to="/inscrire">inscrire</Link></p>
+            <div className="Tab-connexion-form">
+                <h3>Connexion</h3>
+                <div className="Tab-connexion-form-underline">
+                <p>Adresse courriel</p>
+                <input 
+                type="email" 
+                name="mail" 
+                className="input-mail"
+                value={email}
+                onChange={(e) => SetEmail(e.target.value)}
+                ></input>
+                </div>
+                <br></br>
+                <p>Mot de passe</p>
+                <input
+                type="password"
+                name="mot-passe"
+                className="input-mot-passe"
+                value={password}
+                onChange={(e) => SetPassword(e.target.value)}
+                ></input><br></br>
+                <button className="button-connecter">
+                    Se connecter
+                </button><br></br>
+                <p>Aucun compte?</p>
+                <Link to="/inscrire">inscrivez-vous</Link>
+            </div>
         </div>
     );
 }
