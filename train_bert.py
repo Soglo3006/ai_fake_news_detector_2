030791b24e5aee599c1c4235e6ca5aaf56a03a89
Alexandre Booh Louha <ablbooh0@gmail.com>
2025-08-19 11:17:06 -0400

    Ajout du nouveau model entrainé

--- train_bert.py
+++ train_bert.py
@@ -2,6 +2,7 @@ import pandas as pd
 from datasets import Dataset
 from transformers import BertTokenizer, BertForSequenceClassification, Trainer, TrainingArguments
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+from sklearn.model_selection import train_test_split
 import torch
 import transformers
 print("Fichier transformers importé depuis :", transformers.__file__)
@@ -13,12 +14,16 @@ train_df = pd.read_csv("prepared_data/fake_news_train.csv")
 val_df = pd.read_csv("prepared_data/fake_news_val.csv")
 test_df = pd.read_csv("prepared_data/fake_news_test.csv")
 
+train_df, _ = train_test_split(train_df, train_size=8000, stratify=train_df["label"], random_state=42)
+val_df, _ = train_test_split(val_df, train_size=2000, stratify=val_df["label"], random_state=42)
+test_df, _ = train_test_split(test_df, train_size=2000, stratify=test_df["label"], random_state=42)
+
 # === 2. Tokenizer BERT ===
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
 # === 3. Tokenization des données ===
 def tokenize(batch):
-    return tokenizer(batch["content"], padding=True, truncation=True, max_length=512)
+    return tokenizer(batch["content"], padding=True, truncation=True, max_length=128)
 
 train_dataset = Dataset.from_pandas(train_df).map(tokenize, batched=True)
 val_dataset = Dataset.from_pandas(val_df).map(tokenize, batched=True)
@@ -36,9 +41,9 @@ training_args = TrainingArguments(
     output_dir="./results",
     eval_strategy="epoch",
     save_strategy="epoch",
-    per_device_train_batch_size=16,
-    per_device_eval_batch_size=16,
-    num_train_epochs=4,
+    per_device_train_batch_size=8,
+    per_device_eval_batch_size=8,
+    num_train_epochs=2,
     weight_decay=0.01,
     logging_dir="./logs",   
     logging_steps=10,learning_rate=2e-5,
